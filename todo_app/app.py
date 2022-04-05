c12aab91533a592c2488ca954ed16fc325781d9b
David Gowlett <david.gowlett@necsws.com>
2022-04-05 19:16:20 +0100

    add get_items fuction to app.py and added item for loop into idex.html

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import *
+from todo_app.data.session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -9,7 +9,9 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = get_items()
+    #print(items)
+    return render_template('index.html', items=items)
     #return 'Hello World!'
 
 
