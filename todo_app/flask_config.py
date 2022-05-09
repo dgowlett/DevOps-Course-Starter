5a2c3416bf1758f18ca816a808eba16f77e5e5cb
David Gowlett <david.gowlett@necsws.com>
2022-05-09 12:09:33 +0100

    update template with new variables with Trello boardid

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -7,3 +7,12 @@ class Config:
         self.SECRET_KEY = os.environ.get('SECRET_KEY')
         if not self.SECRET_KEY:
             raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
+        self.API_KEY = os.environ.get('TRELLO_API_KEY')
+        if not self.API_KEY:
+            raise ValueError("No TRELLO_API_KEY set for Flask application. Did you follow the setup instructions?")
+        self.API_TOKEN = os.environ.get('TRELLO_API_TOKEN')
+        if not self.API_TOKEN:
+            raise ValueError("No TRELLO_API_TOKEN set for Flask application. Did you follow the setup instructions?")
+        self.BOARD_ID = os.environ.get('TRELLO_BOARDID')
+        if not self.BOARD_ID:
+            raise ValueError("No BOARD_ID set for Flask application. Did you follow the setup instructions?")
