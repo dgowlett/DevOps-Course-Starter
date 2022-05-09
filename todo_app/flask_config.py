42b6cdc99dc2b560204be145888e4c1017b33507
David Gowlett <david.gowlett@necsws.com>
2022-05-09 12:20:05 +0100

    README update

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -13,6 +13,6 @@ class Config:
         self.API_TOKEN = os.environ.get('TRELLO_API_TOKEN')
         if not self.API_TOKEN:
             raise ValueError("No TRELLO_API_TOKEN set for Flask application. Did you follow the setup instructions?")
-        self.BOARD_ID = os.environ.get('TRELLO_BOARDID')
+        self.BOARD_ID = os.environ.get('TRELLO_BOARD_ID')
         if not self.BOARD_ID:
             raise ValueError("No BOARD_ID set for Flask application. Did you follow the setup instructions?")
