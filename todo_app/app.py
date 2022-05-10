d021e5df026399ed751b4b769158b3912d9dbd17
David Gowlett <david.gowlett@necsws.com>
2022-05-10 08:28:13 +0100

    updated todo_app/data/trello_items.py

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, redirect, render_template
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items, add_item
+from todo_app.data.session_items import add_item
+from todo_app.data.trello_items import get_items
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -9,7 +10,7 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    items = get_items()
+    items = get_items(app.config["BOARD_ID"],app.config["API_KEY"],app.config["API_TOKEN"])
     return render_template('index.html', items=items)
     
 @app.route('/additem', methods=['POST'])
@@ -17,6 +18,7 @@ def addnewtodo():
     newtodo= request.form.get('new_item')
     if newtodo != '':
         add_item(newtodo)
+        print (app.config["BOARD_ID"])
     return redirect('/')
 
 
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,1 +1,35 @@
 import requests
+
+_DEFAULT_ITEMS = [
+    { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items' },
+    { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added' }
+]
+
+def get_items(BOARD_ID,APIKey,APIToken):
+    """
+    Fetches all saved items from trello board_id.
+
+    Args:
+        id: The BOARD_ID of the trello board and API_KEY, API_TOKEN.
+
+    Returns:
+        list: The list of cards from a list/board.
+    """
+    query = {
+        'key': APIKey,
+        'token': APIToken
+    }
+
+    print(query)
+    url = "https://api.trello.com/1/boards/" + BOARD_ID + "/cards"
+    print(url)
+    response = requests.request("GET",url,params=query)
+
+    jsonResponse = response.json()
+    print(jsonResponse)
+    for dataline in jsonResponse:
+        for key, value in dataline.items():
+            if key == 'name':
+                print(key, ":", value)
+    #return session.get('name', _DEFAULT_ITEMS.copy())
+    return next((item for item in jsonResponse), None)
