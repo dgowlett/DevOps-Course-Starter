3d350afeb3875f2d5555d7a8a32aba1c40f9dfa0
David Gowlett <david.gowlett@necsws.com>
2022-05-11 18:37:59 +0100

    added actions for buttons

--- todo_app/app.py
+++ todo_app/app.py
@@ -22,18 +22,20 @@ def addnewtodo():
 
 @app.route('/itemaction', methods=['POST'])
 def actiontodo():
-    option = request.form['To Do List']
-    print("Option =["+option+"]")
+
+    if 'To Do List' in request.form:
+        option = request.form['To Do List']
+        print("Option =["+option+"]")
 
 
-    if request.form.get('Action 1') == 'Started':
-        print('Started Card')
-    elif request.form.get('Action 2') == 'Not Started':
-        print('Not Started Card')
-    elif request.form.get('Action 3') == 'Completed':
-        print('Completed')
-    elif request.form.get('Action 4') == 'delete':
-        print('delete')
+        if request.form.get('Action 1') == 'Started':
+            print('Started Card')
+        elif request.form.get('Action 2') == 'Not Started':
+            print('Not Started Card')
+        elif request.form.get('Action 3') == 'Completed':
+            print('Completed')
+        elif request.form.get('Action 4') == 'delete':
+            print('delete')
     #newtodo= request.form.get('new_item')
     #if newtodo != '':
     #    add_item(newtodo,app.config["BOARD_ID"],app.config["API_KEY"],app.config["API_TOKEN"])
