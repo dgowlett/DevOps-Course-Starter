c07769cec880dfbb28dc8b0812d482421f04052f
EC2 Default User <ec2-user@ip-10-0-0-59.eu-west-2.compute.internal>
2022-07-29 22:12:51 +0000

    flask config

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -4,9 +4,6 @@ import os
 class Config:
     def __init__(self):
         """Base configuration variables."""
-        self.SECRET_KEY = os.environ.get('SECRET_KEY')
-        if not self.SECRET_KEY:
-            raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
         self.API_KEY = os.environ.get('TRELLO_API_KEY')
         if not self.API_KEY:
             raise ValueError("No TRELLO_API_KEY set for Flask application. Did you follow the setup instructions?")
