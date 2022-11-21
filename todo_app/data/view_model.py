8201f9c2e561a7b920ad9376c9fef4afbde81b37
dgowlett <101567346+dgowlett@users.noreply.github.com>
2022-11-21 16:18:27 +0000

    Writing tests

--- todo_app/data/view_model.py
+++ todo_app/data/view_model.py
@@ -1,7 +1,17 @@
+from todo_app.data.Item_class import Item
+
 class ViewModel:
-    def __init__(self, items):
+    def __init__(self, items: list[Item]):
         self._items = items
  
     @property
-    def items(self):
+    def items(self) -> list[Item]:
         return self._items
+        
+    @property
+    def todo_items(self) -> list[Item]:
+        return []
+
+    @property
+    def done_items(self) -> list[Item]:
+        return []
