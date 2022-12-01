597cab4dadb7ac0b0ca773f404aef6f25d4acebb
dgowlett <101567346+dgowlett@users.noreply.github.com>
2022-12-01 16:56:07 +0000

    Add test file

---
+++ test/test_view_model.py
@@ -0,0 +1,18 @@
+from todo_app.data.view_model import ViewModel
+from todo_app.data.Item_class import Item
+
+def test_done_items_property_only_returns_the_done_items():
+    # Arrange
+    items = [
+        Item('0','0','test1','/test1'), # Done Item 
+        Item('1','1','test2','/test2')  # Todo Item 
+    ]
+    view_model = ViewModel(items)
+
+    # Act
+    returned_done_items = view_model.done_items
+
+    # Assert
+    assert len(returned_done_items) == 2
+    done_item = returned_done_items[0]
+    assert done_item.status == "Done"
