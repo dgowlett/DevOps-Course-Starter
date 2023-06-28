5dbe2bf50b23d7892f272723f0de5e79ed7bfce5
dgowlett <david.gowlett@necsws.com>
2023-06-28 20:56:34 +0100

    e2e tests

---
+++ todo_app/tests_e2e/test_client_e2e.py
@@ -0,0 +1,136 @@
+import os
+import pytest
+import requests
+import json
+from threading import Thread
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from dotenv import load_dotenv
+from todo_app import app
+
+
+@pytest.fixture(scope='module')
+def app_with_temp_board():
+    # Load our real environment variables
+    load_dotenv(override=True)
+
+    # Create the new board & update the board id environment variable
+    board_id = create_trello_board()
+    os.environ['TRELLO_BOARD_ID'] = board_id
+
+    # Construct the new application
+    application = app.create_app()
+
+    # Start the app in its own thread.
+    thread = Thread(target=lambda: application.run(use_reloader=False))
+    thread.daemon = True
+    thread.start()
+    
+    # Give the app a moment to start
+    sleep(1)
+
+    # Return the application object as the result of the fixture
+    yield application
+
+    # Tear down
+    thread.join(1)
+    delete_trello_board(board_id)
+
+def create_trello_board():
+
+    # TODO Create a new board in Trello and return the id
+    query = {   
+        'key': os.environ.get('TRELLO_API_KEY'),
+        'token': os.environ.get('TRELLO_API_TOKEN')
+    }
+    
+    url = "https://api.trello.com/1/boards/?name=e2eTesting"
+    response = requests.request("POST",url,params=query)
+    jsonResponse = response.json()
+
+    return jsonResponse["shortUrl"].split("/")[4]
+
+def delete_trello_board(board_id):
+
+    # TODO Delete the Trello board with id board_id
+    query = {   
+        'key': os.environ.get('TRELLO_API_KEY'),
+        'token': os.environ.get('TRELLO_API_TOKEN')
+    }
+
+    url = "https://api.trello.com/1/boards/" + board_id
+    response = requests.request("DELETE",url,params=query)
+
+    return None
+
+@pytest.fixture(scope="module")
+def driver():
+    with webdriver.Firefox() as driver:
+        yield driver
+
+def test_task_journey(driver, app_with_temp_board):
+    driver.get('http://localhost:5000/')
+
+    print(dir(driver))
+    assert driver.title == 'To-Do App'
+    driver.implicitly_wait(5)
+
+    test_text = "Happy Testing"
+    todo_text_field = driver.find_element(By.NAME, 'new_item')
+    todo_text_field.send_keys(test_text)
+    sleep(3)
+
+    driver.find_element(By.NAME, "submit").click()
+    sleep(1)
+
+    test_text = "Happy Testing2"
+    todo_text_field = driver.find_element(By.NAME, 'new_item')
+    todo_text_field.send_keys(test_text)
+    sleep(1)
+
+    driver.find_element(By.NAME, "submit").click()
+    sleep(4)
+
+def test_task_journey_complete(driver, app_with_temp_board):
+    driver.get('http://localhost:5000/')
+
+    assert driver.title == 'To-Do App'
+    driver.implicitly_wait(5)
+
+    # Click on Not started button to complete
+    driver.find_element(By.NAME, "todo_0").click()
+    sleep(1)
+
+    driver.implicitly_wait(5)
+    
+    # Click on Not started button to complete
+    driver.find_element(By.NAME, "todo_0").click()
+    sleep(1)
+
+def test_task_journey_notstarted(driver, app_with_temp_board):
+    driver.get('http://localhost:5000/')
+
+    assert driver.title == 'To-Do App'
+    driver.implicitly_wait(5)
+    
+    # Click on Completed button to change to Not started
+    driver.find_element(By.NAME, "done_0").click()
+    sleep(1)
+
+def test_task_journey_delete(driver, app_with_temp_board):
+    driver.get('http://localhost:5000/')
+
+    assert driver.title == 'To-Do App'
+    driver.implicitly_wait(5)
+    
+    # Delete Todo item found in Not Started
+    driver.find_element(By.NAME, "todo_delete_0").click()
+    sleep(1)
+
+    # Delete Todo item found in Completed
+    driver.find_element(By.NAME, "done_delete_0").click()
+    sleep(1)
+
+
